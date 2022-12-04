5c5511e1ef580cca1cc85e954d48075f8f731abe
Susant Paudel <techbizznepal@gmail.com>
2022-12-04 12:03:22 +0545

    query page

--- pages/query.jsx
+++ pages/query.jsx
@@ -22,8 +22,8 @@ export default Query;
 
 const SearchingProcessComponent = ({service, lat, long}) => {
     return <div className={`flex justify-center items-center h-screen flex-col gap-y-2`}>
-        <h1 className={`text-3xl`}>Searching for <span
-            className={`text-2xl md:text-4xl  lg:text-5xl text-blue-600 font-semibold`}>{service}...</span></h1>
+        <h1 className={`text-2xl md:text-4xl  lg:text-5xl `}>Searching for <span
+            className={`text-3xl md:text-5xl  lg:text-6xl text-blue-600 font-semibold`}>{service}...</span></h1>
         <p>{`for location ${lat}, ${long}`}</p>
     </div>
 }
