d686c19974864ce93e79a3d62c30da97355154b7
Susant Paudel <techbizznepal@gmail.com>
2022-12-04 11:50:42 +0545

    list page

--- pages/service-list.jsx
+++ pages/service-list.jsx
@@ -5,8 +5,8 @@ const ServiceList = () => {
     const router = useRouter()
     const {query} = router
     const {service, loc} = query
-    const handleServiceClick = (e) => {
-        console.log(e)
+    const handleServiceClick = (e, service) => {
+        alert(`you have pinged ${service}`)
     }
     return (
         <>
@@ -16,13 +16,13 @@ const ServiceList = () => {
             <div className={`flex flex-col justify-center items-center h-64`}>
                 <div className={`text-6xl`}>{`5 results found for ${service}...`}</div>
             </div>
-            <div className={`flex flex-col justify-center items-center`}>
-                <div className={`grid grid-cols-3 items-center gap-6`}>
+            <div className={`container mx-auto`}>
+                <div className={`flex flex-col gap-6  w-full`}>
                     {
                         ['service1', 'service2', 'service3', 'service4', 'service5', 'service6']
                             .map(service =>
                                 <ServiceComponent key={service}
-                                                  value={service}
+                                                  service={service}
                                                   loc={loc}
                                                   handleClick={handleServiceClick}
                                 />
@@ -36,17 +36,22 @@ const ServiceList = () => {
 }
 export default ServiceList
 
-const ServiceComponent = ({value, loc, handleClick}) => {
-    console.log(loc)
+const ServiceComponent = ({service, loc, handleClick, handleViewClick}) => {
     return (
-        <div onClick={handleClick}
-             title={value}
-             className={`w-64 bg-slate-200 hover:bg-slate-400 text-white flex flex-col items-center gap-y-8 py-8 rounded-xl`}
+        <div
+            title={service}
+            className={`px-8 bg-blue-400  text-white flex flex-col gap-y-8 py-8 rounded-xl select-none `}
         >
-            <div className={`text-4xl`} title={value} id={`title`}>{value}</div>
-            <div title={value} id={`description`}>{`description`}</div>
-            <div title={`ping him!`} id={`distance`}>
-                <button className={`cursor-pointer text-2xl px-4 py-2 bg-green-600 rounded-xl`}>Ping him!</button>
+            <div className={`text-4xl`} title={service} id={`title`}>{service.toUpperCase()}</div>
+            <div title={service} id={`description`}>{`description`}</div>
+            <div title={service} id={`description`}>{loc}</div>
+            <div className={`flex gap-3`} title={`ping him!`} id={`distance`}>
+                <button onClick={(e) => handleClick(e, service)}
+                        className={`cursor-pointer text-2xl px-4 py-2 bg-green-600 rounded-xl`}>Ping him!
+                </button>
+                <button onClick={(e) => console.log(e, service)}
+                        className={`cursor-pointer text-2xl px-4 py-2 bg-yellow-600 rounded-xl`}>Check on him!
+                </button>
             </div>
         </div>
     )
