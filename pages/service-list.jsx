6160324308b4055f27be16f1889988c55ab297ed
Susant Paudel <techbizznepal@gmail.com>
2022-12-04 12:13:58 +0545

    list page

--- pages/service-list.jsx
+++ pages/service-list.jsx
@@ -13,11 +13,11 @@ const ServiceList = () => {
             <Head>
                 <title>{`5 result found for ${service}`}</title>
             </Head>
-            <div className={`flex flex-col justify-center items-center h-64`}>
+            <div className={`flex flex-col justify-center items-center h-64 md:mx-auto  mx-4`}>
                 <div className={`text-6xl`}>{`5 results found for ${service}...`}</div>
             </div>
-            <div className={`container mx-auto`}>
-                <div className={`flex flex-col gap-6  w-full`}>
+            <div className={`container md:mx-auto mx-4`}>
+                <div className={`flex flex-col gap-2 w-full`}>
                     {
                         ['service1', 'service2', 'service3', 'service4', 'service5', 'service6']
                             .map(service =>
@@ -40,17 +40,18 @@ const ServiceComponent = ({service, loc, handleClick, handleViewClick}) => {
     return (
         <div
             title={service}
-            className={`px-8 bg-gradient-to-r from-blue-400 from-blue-600 text-white flex flex-col gap-y-8 py-8 rounded-xl select-none `}
+            className={`px-8 bg-gradient-to-r from-blue-400 from-blue-600 text-white flex flex-col gap-y-4 md:gap-y-8 py-8 rounded-xl select-none `}
         >
-            <div className={`text-4xl`} title={service} id={`title`}>{service.toUpperCase()}</div>
-            <div title={service} id={`description`}>{`description`}</div>
-            <div title={service} id={`description`}>{loc}</div>
+            <div className={`text-2xl md:text-4xl`} title={service} id={`title`}>{service.toUpperCase()}</div>
+            <div className={`text-md md:text-lg`} title={service} id={`description`}>{`description`}</div>
+            <div className={`text-md md:text-lg`} title={service} id={`description`}>{loc}</div>
+
             <div className={`flex gap-3`} title={`ping him!`} id={`distance`}>
                 <button title={`Ping him!`} onClick={(e) => handleClick(e, service)}
-                        className={`cursor-pointer text-2xl px-4 py-2 bg-green-600 rounded-xl`}>Ping him!
+                        className={`cursor-pointer text-md md:text-xl px-4 py-2 bg-green-600 rounded-xl`}>Ping him!
                 </button>
                 <button title={ `Check on him!`} onClick={(e) => console.log(e, service)}
-                        className={`cursor-pointer text-2xl px-4 py-2 bg-yellow-600 rounded-xl`}>Check on him!
+                        className={`cursor-pointer text-md md:text-xl px-4 py-2 bg-yellow-600 rounded-xl`}>Check on him!
                 </button>
             </div>
         </div>
