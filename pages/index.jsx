9d5c7bdd2f2633509218756ea31be7d49e734fd9
Susant Paudel <techbizznepal@gmail.com>
2022-12-04 08:29:40 +0545

    home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import HomePageSearchComponent from "../components/home-page-search-component";
 import {useRef} from "react";
+import Head from "next/head";
+
 
 if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
     window.navigator.geolocation.getCurrentPosition(
@@ -13,13 +15,18 @@ if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
 const Home = () => {
     const inputRef = useRef()
     return (
-        <div className={`flex justify-center items-center h-screen flex-col  gap-y-16`}>
-            <div className={`flex justify-center items-center w-full gap-y-2 gap-x-2 `}>
-                <h1 className={`text-2xl lg:text-6xl md:text-4xl  font-semibold text-blue-400`}>Choose 100+ Services around you.</h1>
-            </div>
+        <>
+            <Head>
+                <title>Gravity Services</title>
+            </Head>
+            <div className={`flex justify-center items-center h-screen flex-col  gap-y-16`}>
+                <div className={`flex justify-center items-center w-full gap-y-2 gap-x-2 `}>
+                    <h1 className={`text-2xl lg:text-6xl md:text-4xl  font-semibold text-blue-400`}>Choose 100+ Services around you.</h1>
+                </div>
 
-            <HomePageSearchComponent inputRef={inputRef} placeholder={`Sydney, Australia`} />
-        </div>
+                <HomePageSearchComponent inputRef={inputRef} placeholder={`Sydney, Australia`} />
+            </div>
+        </>
     )
 }
 export default Home
