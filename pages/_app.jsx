1b787cee90f35b5966fcb9cdf3f17a9b0163f57b
Susant Paudel <techbizznepal@gmail.com>
2022-12-04 08:19:25 +0545

    home page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,7 @@
-
-import {CssBaseline, NextUIProvider} from "@nextui-org/react";
-import './../styles/Home.module.css'
+import './../styles/globals.css'
 function MyApp({Component, pageProps}) {
     return (
-            <NextUIProvider>
-                {CssBaseline.flush()}
-                <Component {...pageProps} />
-            </NextUIProvider>
+        <Component {...pageProps} />
     )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,83 +1,25 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import {Button, Container, Row, Spacer, Text} from "@nextui-org/react";
-import {useState} from "react";
-import {useRouter} from "next/router";
+import HomePageSearchComponent from "../components/home-page-search-component";
+import {useRef} from "react";
 
+if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+            console.log(position)
+        },
+        (positionError) => {
+            console.log(positionError)
+        })
+}
 const Home = () => {
-    const router = useRouter()
-    const [query, setQuery] = useState("");
-    const [homePageData, setHomePageData] = useState({
-        appName: "Gravity Services",
-        searchButtonText: "Search Service",
-        searchButtonProgressText: "Searching...",
-        footerText: "Powered By Techbizz"
-    });
-    const handleSearchClick = () => {
-        const dd = {...homePageData}
-        dd["searchButtonText"] = homePageData.searchButtonProgressText
-        setHomePageData(dd)
-
-
-        setTimeout(() => {
-            const dd = {...homePageData}
-            dd["searchButtonText"] = "Search Service"
-            setHomePageData(dd)
-            router.push('search')
-        }, 3000)
-
-
-    }
+    const inputRef = useRef()
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>{homePageData.appName}</title>
-                <meta name="description" content="get local services "/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+        <div className={`flex justify-center items-center h-screen flex-col  gap-y-16`}>
+            <div className={`flex justify-center items-center w-full gap-y-2 gap-x-2 `}>
+                <h1 className={`text-6xl font-semibold text-blue-400`}>Choose 100+ Services around you.</h1>
+            </div>
 
-            <main className={styles.main}>
-
-
-                <Container>
-                    {/*<Card css={{$$cardColor: '$colors$primary'}}>*/}
-                    <Row justify="center" align="center">
-                        <Text size={"2em"}>Welcome to</Text>
-                    </Row>
-                    <Row justify={`center`} align={`center`}>
-                        <Text size={"5em"} h1={true} color={'#1b7bec'}>{homePageData.appName}</Text>
-                        {/*<h1 className={styles.title}>*/}
-
-                        {/*</h1>*/}
-                    </Row>
-                    <Spacer y={3}/>
-                    <Row justify="center" align="center">
-                        <SelectBox />
-                        {/*<Input value={query} name={'query'} size={"xl"}*/}
-                        {/*       label={"mechanics, taxi etc..."}/>*/}
-                    </Row>
-                    <Spacer y={3}/>
-                    <Row justify="center" align="center">
-                        <Button onPress={() => handleSearchClick()}
-                                size={"md"}
-                                color={'primary'}>{homePageData.searchButtonText}</Button>
-                    </Row>
-                </Container>
-            </main>
-
-            <footer className={styles.footer}>
-                <Text>{homePageData.footerText}</Text>
-            </footer>
+            <HomePageSearchComponent inputRef={inputRef} placeholder={`Sydney, Australia`} />
         </div>
     )
 }
-
 export default Home
-export const SelectBox = () => {
-    return (
-        <select style={{'width':"40vw", "height":"4vh"}}>
-            <option value={`mechanics`}>Mechanics</option>
-            <option value={`plumber`}>Plumber</option>
-        </select>
-    )
-}
